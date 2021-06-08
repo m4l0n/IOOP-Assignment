b202caf215c0e591ea0439d5028b2a335a5b75d7
ANG RU XIAN <TP062253@mail.apu.edu.my>
2021-06-08 14:03:30 +0800

    SQLQuery

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,2 +1,13 @@
-﻿INSERT INTO [dbo].[User]
-Values ('TP062253', 'Nigel', 'nigel@gmail.com', 'nigeltesting', 'Student');
+﻿Declare @date Date, @days INT = 6, @counter INT = 1
+Declare @Results TABLE (Date Date, TotalReservation int)
+WHILE (@counter < @days)
+BEGIN
+	Declare @currentDate Date = DateAdd(Day, -@counter, getdate())
+	insert into @Results (Date, TotalReservation)
+	select Date, COALESCE(count(ReservationID),0) as TotalReservation
+	from [dbo].Reservation
+	where Date=@currentDate
+	group by Date
+	Set @counter += 1
+END
+select * from @Results
